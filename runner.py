8f51e4f1dbbb0277af6798a652f003a307a5eaf1
1KrishnaSingh346 <145567074+1KrishnaSingh346@users.noreply.github.com>
2024-08-25 12:34:41 +0530

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import cv2
+import mediapipe as mp
+import math
+import statistics
+import net_init
+import numpy as np
+
 
 # Load the model
 # Assuming app.py contains the necessary functions for processing
